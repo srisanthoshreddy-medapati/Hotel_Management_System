b7c20a0841a4b09574519958192edfcd23743160
Sri Santhosh Reddy Medapati <86189839+srisanthoshreddy-medapati@users.noreply.github.com>
2022-05-07 21:14:35 +0530

    HMS_FINAL

---
+++ failed.php
@@ -0,0 +1,10 @@
+<html>
+<body  bgcolor="green"  text="white">
+<h1 align="center">Invald PhoneNumber or Password</h1>
+<form action="user_login.php" method="post">
+<div style="text-align:center">  
+    <input type="submit" value="Return to User Login" class="submit">  
+</div>  
+</form>
+</body>
+</html>
---
+++ failed1.php
@@ -0,0 +1,10 @@
+<html>
+<body  bgcolor="green"  text="white">
+<h1 align="center">Invald UserId or Password</h1>
+<form action="admin_login.php" method="post">
+<div style="text-align:center">  
+    <input type="submit" value="Return to Admin Login" class="submit">  
+</div>  
+</form>
+</body>
+</html>
